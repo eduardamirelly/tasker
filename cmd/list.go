d772183d30f3e4c22917ded9339a96c3d78adba9
eduardamirelly <mirelly.eduarda404@gmail.com>
2025-09-11 21:47:25 -0300

    feat: list not found results

--- cmd/list.go
+++ cmd/list.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"database/sql"
 	"fmt"
 
 	"github.com/eduardamirelly/tasker/database"
@@ -19,30 +18,11 @@ var listCmd = &cobra.Command{
 			fmt.Printf("Error listing tasks: %v\n", err)
 			return
 		}
-		defer result.Close()
-		for result.Next() {
-			var task models.Task
-			err := result.Scan(&task.ID, &task.Title, &task.Description, &task.Done, &task.CreatedAt, &task.CompletedAt)
-			if err != nil {
-				fmt.Printf("Error scanning task: %v\n", err)
-				return
-			}
-			done := "✅"
-			if !task.Done {
-				done = "❌"
-			}
-
-			createdAt := task.CreatedAt.Format("2006-01-02 15:04:05")
-			completedAt := "N/A"
-			if task.CompletedAt != nil {
-				completedAt = task.CompletedAt.Format("2006-01-02 15:04:05")
-			}
-			fmt.Printf("%v %v - %v\n", done, task.ID, task.Title)
-			fmt.Printf("Description: %v\n", task.Description)
-			fmt.Printf("Created At: %v\n", createdAt)
-			fmt.Printf("Completed At: %v\n", completedAt)
-			fmt.Println("--------------------------------")
+		if len(result) == 0 {
+			emptyTasks()
+			return
 		}
+		printTasks(result)
 	},
 }
 
@@ -50,12 +30,46 @@ func init() {
 	rootCmd.AddCommand(listCmd)
 }
 
-func listTasks() (*sql.Rows, error) {
+func listTasks() ([]models.Task, error) {
 	query := `SELECT id, title, description, done, created_at, completed_at FROM tasks`
-	result, err := database.DB.Query(query)
+	rows, err := database.DB.Query(query)
 	if err != nil {
-		fmt.Printf("Error listing tasks: %v\n", err)
 		return nil, err
 	}
-	return result, err
+	defer rows.Close()
+
+	var tasks []models.Task
+	for rows.Next() {
+		var task models.Task
+		err := rows.Scan(&task.ID, &task.Title, &task.Description, &task.Done, &task.CreatedAt, &task.CompletedAt)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func emptyTasks() {
+	fmt.Println("No tasks found")
+}
+
+func printTasks(tasks []models.Task) {
+	for _, task := range tasks {
+		done := "✅"
+		if !task.Done {
+			done = "❌"
+		}
+		createdAt := task.CreatedAt.Format("2006-01-02 15:04:05")
+		completedAt := "N/A"
+		if task.CompletedAt != nil {
+			completedAt = task.CompletedAt.Format("2006-01-02 15:04:05")
+		}
+		fmt.Printf("%v %v - %v\n", done, task.ID, task.Title)
+		fmt.Printf("Description: %v\n", task.Description)
+		fmt.Printf("Created At: %v\n", createdAt)
+		fmt.Printf("Completed At: %v\n", completedAt)
+		fmt.Println("--------------------------------")
+	}
 }
